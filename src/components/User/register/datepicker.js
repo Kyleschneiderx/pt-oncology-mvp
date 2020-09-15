3f833bbc5eb001bd4c7666be700af606bbdec08e
Kyle Schneider <kyleschneider@Kyles-MacBook-Air.local>
2020-09-15 07:39:17 -0700

    update

--- src/components/User/register/datepicker.js
+++ src/components/User/register/datepicker.js
@@ -1,41 +1,41 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { DatePicker } from 'material-ui-pickers'
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import { DatePicker } from 'material-ui-pickers'
 
-const DatePickerField = ({
-  inputFormat,
-  dateParser,
-  input,
-  label,
-  meta: { touched, invalid, error },
-  dateFormatter,
-  ...rest,
-}) => {
-  return (
-    <DatePicker
-      error={touched && invalid}
-      format={inputFormat}
-      helperText={touched && error}
-      label={label}
-      onChange={val => input.onChange(dateParser(val))}
-      value={dateFormatter(input.value)}
-      {...rest}  />
-  )
-}
+// const DatePickerField = ({
+//   inputFormat,
+//   dateParser,
+//   input,
+//   label,
+//   meta: { touched, invalid, error },
+//   dateFormatter,
+//   ...rest,
+// }) => {
+//   return (
+//     <DatePicker
+//       error={touched && invalid}
+//       format={inputFormat}
+//       helperText={touched && error}
+//       label={label}
+//       onChange={val => input.onChange(dateParser(val))}
+//       value={dateFormatter(input.value)}
+//       {...rest}  />
+//   )
+// }
 
-DatePickerField.propTypes = {
-  dateFormatter: PropTypes.func,
-  dateParser: PropTypes.func,
-  input: PropTypes.object.isRequired,
-  inputFormat: PropTypes.string,
-  label: PropTypes.string.isRequired,
-  meta: PropTypes.object.isRequired,
-}
+// DatePickerField.propTypes = {
+//   dateFormatter: PropTypes.func,
+//   dateParser: PropTypes.func,
+//   input: PropTypes.object.isRequired,
+//   inputFormat: PropTypes.string,
+//   label: PropTypes.string.isRequired,
+//   meta: PropTypes.object.isRequired,
+// }
 
-DatePickerField.defaultProps = {
-  dateFormatter: v => v.replace(/-/g, '/'),
-  dateParser: v => v.getFullYear() + '-' + ('0' + (v.getMonth() + 1)).slice(-2) + '-' + ('0' + v.getDate()).slice(-2),
-  inputFormat: 'dd/MM/yyyy',
-}
+// DatePickerField.defaultProps = {
+//   dateFormatter: v => v.replace(/-/g, '/'),
+//   dateParser: v => v.getFullYear() + '-' + ('0' + (v.getMonth() + 1)).slice(-2) + '-' + ('0' + v.getDate()).slice(-2),
+//   inputFormat: 'dd/MM/yyyy',
+// }
 
-export default DatePickerField
+// export default DatePickerField
